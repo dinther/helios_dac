4f72f58c079a09b2bb6c171952419aa64fecc6de
Paul <pvdinther@gmail.com>
2024-07-18 23:23:20 +1200

    update

--- helios_dac.js
+++ helios_dac.js
@@ -21,15 +21,15 @@ const HELIOS_FAIL = 0;     //  A valid test result
 const HELIOS_ERROR = -1;   //  Abnormal fail
 
 const HELIOS_FLAGS_DEFAULT = 0;
-const HELIOS_FLAGS_START_IMMEDIATELY = (1 << 0);
-const HELIOS_FLAGS_SINGLE_MODE = (1 << 1);
-//  export const HELIOS_FLAGS_DONT_BLOCK = (1 << 2); //not applicable to javascript. Everything is non blocking
+const HELIOS_FLAGS_START_IMMEDIATELY = (1 << 0); //  start output immediately, instead of waiting for current frame (if there is one) to finish playing
+const HELIOS_FLAGS_SINGLE_MODE = (1 << 1);       //  play frame only once, instead of repeating until another frame is written
+//const HELIOS_FLAGS_DONT_BLOCK = (1 << 2);      //  not applicable to javascript. Everything is non blocking
 
 // USB properties
 const HELIOS_VID = 0x1209;
 const HELIOS_PID = 0xE500;
 
-const MAX_GET_STATUS_RETRIES = 512;
+const MAX_GET_STATUS_RETRIES = 3; //512;
 
 const EP_BULK_OUT = 0x02;
 const EP_BULK_IN = 0x81;
@@ -48,8 +48,6 @@ const HELIOS_STATUS_RESPONSE_CODE = 0x83;
 const HELIOS_FIRMWARE_VERSION_RESPONSE_CODE = 0x84;
 const HELIOS_GET_NAME_RESPONSE_CODE = 0x85;
 
-//const EP_INT_IN = 0x83;  // this is a command response code?
-
 // Point data structure
 export class HeliosPoint {
     constructor(x = 0, y = 0, r = 0, g = 0, b = 0, i) {
@@ -64,7 +62,11 @@ export class HeliosPoint {
 
 export async function connectHeliosDevice(){
     let usbDevice = await navigator.usb.requestDevice({ filters: [{vendorId: HELIOS_VID, classCode: HELIOS_PID}]});
-    return (usbDevice)? new HeliosDevice(usbDevice) : null;
+    if (usbDevice){
+        let heliosDevice =  new HeliosDevice(usbDevice);
+        return heliosDevice;
+    }
+    return null
 }
 
 export async function getHeliosDevices(){
@@ -79,52 +81,6 @@ export async function getHeliosDevices(){
     return heliosDevices;
 }
 
-/*
-export class HeliosDacManager {
-    constructor() {
-        this.initialized = false;
-        this.deviceList = [];
-    }
-
-    async openDevices() {
-        if (this.initialized) return this.deviceList.length;
-
-        this.deviceList = [];
-        try {
-            let devices = await navigator.usb.getDevices();
-            for (const device of devices) {
-                if (device.vendorId === HELIOS_VID && device.productId === HELIOS_PID) {
-                    await device.open();
-                    await device.selectConfiguration(1);
-                    await device.claimInterface(0);
-                    await device.selectAlternateInterface(0,1);
-                    let dacDevice = new HeliosDacDevice(device);
-                    await dacDevice.getFirmwareVersion();
-                    await dacDevice.getName();
-                    this.deviceList.push(dacDevice);
-                }
-            }
-            this.initialized = true;
-            return this.deviceList.length;
-        } catch (error) {
-            console.error('Error opening devices:', error);
-            return -1;
-        }
-    }
-
-    async closeDevices() {
-        if (!this.initialized) return HELIOS_ERROR;
-
-        this.initialized = false;
-        for (const device of this.deviceList) {
-            await device.close();
-        }
-        this.deviceList = [];
-        return HELIOS_SUCCESS;
-    }
-}
-    */
-
 export class HeliosDevice{
     #running = false;
     #firmwareVersion;
@@ -135,9 +91,8 @@ export class HeliosDevice{
         this.closed = true;
         this.#firmwareVersion = 0;
         this.#name = '';
-        this.frameBuffer = new ArrayBuffer(HELIOS_MAX_POINTS * 7 + 5);
         this.onFrame = null;
-        //this.init();
+        this.frameBuffer = new Uint8Array(HELIOS_MAX_POINTS * 7 + 5);
     }
 
     #DataViewToString(dataView, offset=1){
@@ -161,8 +116,6 @@ export class HeliosDevice{
         this.closed = false;
         await this.#getFirmwareVersion();
         await this.#getName();
-        //this.frameHandler();
-        //this.handleRunning(); 
     }
 
     async sendControl(buffer, receiveLength){
@@ -176,6 +129,7 @@ export class HeliosDevice{
     }
 
     async sendFrame(points=null, pps=30000, singleShot=false, interuptFrame=false) {
+        if (this.closed) return HELIOS_ERROR;
         if ( points == null ||
              points.length > HELIOS_MAX_POINTS ||
              pps > HELIOS_MAX_RATE ||
@@ -184,7 +138,6 @@ export class HeliosDevice{
              this.frameReady ) return HELIOS_ERROR;
 
         let bufPos = 0;
-        const _frameBuffer = new Uint8Array(points.length * 7 + 5);
 
         let ppsActual = pps;
         let numOfPointsActual = points.length;
@@ -197,33 +150,21 @@ export class HeliosDevice{
         if (singleShot) flags = flags|HELIOS_FLAGS_SINGLE_MODE;
         if (interuptFrame) flags = flags|HELIOS_FLAGS_START_IMMEDIATELY;
         for (let i = 0; i < points.length; i++) {
-            _frameBuffer[bufPos++] = points[i].x >> 4;
-            _frameBuffer[bufPos++] = ((points[i].x & 0x0F) << 4) | (points[i].y >> 8);
-            _frameBuffer[bufPos++] = points[i].y & 0xFF;
-            _frameBuffer[bufPos++] = points[i].r;
-            _frameBuffer[bufPos++] = points[i].g;
-            _frameBuffer[bufPos++] = points[i].b;
-            _frameBuffer[bufPos++] = points[i].i;
+            this.frameBuffer[bufPos++] = points[i].x >> 4;
+            this.frameBuffer[bufPos++] = ((points[i].x & 0x0F) << 4) | (points[i].y >> 8);
+            this.frameBuffer[bufPos++] = points[i].y & 0xFF;
+            this.frameBuffer[bufPos++] = points[i].r;
+            this.frameBuffer[bufPos++] = points[i].g;
+            this.frameBuffer[bufPos++] = points[i].b;
+            this.frameBuffer[bufPos++] = points[i].i;
         }
-        _frameBuffer[bufPos++] = ppsActual & 0xFF;
-        _frameBuffer[bufPos++] = ppsActual >> 8;
-        _frameBuffer[bufPos++] = numOfPointsActual & 0xFF;
-        _frameBuffer[bufPos++] = numOfPointsActual >> 8;
-        _frameBuffer[bufPos++] = flags;
-
-        //if (flags & HELIOS_FLAGS_DONT_BLOCK) {
-        //    this.frameReady = true;
-         //   return HELIOS_SUCCESS;
-        //} else {
-        return await this.doFrame(_frameBuffer);
-        //}
-    }
-
-    async doFrame(_frameBuffer) {
-        if (this.closed) return HELIOS_ERROR;
-
+        this.frameBuffer[bufPos++] = ppsActual & 0xFF;
+        this.frameBuffer[bufPos++] = ppsActual >> 8;
+        this.frameBuffer[bufPos++] = numOfPointsActual & 0xFF;
+        this.frameBuffer[bufPos++] = numOfPointsActual >> 8;
+        this.frameBuffer[bufPos++] = flags;
         try {
-            await this.usbDevice.transferOut(EP_BULK_OUT, _frameBuffer);
+            await this.usbDevice.transferOut(EP_BULK_OUT, this.frameBuffer.slice(0,bufPos));
             return HELIOS_SUCCESS;
         } catch (error) {
             console.error('Error sending frame:', error);
@@ -231,34 +172,16 @@ export class HeliosDevice{
         }
     }
 
-    /*
-    async frameHandler() {
-        while (!this.closed) {
-            if (this.frameReady) {
-                await this.doFrame();
-                this.frameReady = false;
-            }
-            await new Promise(resolve => setTimeout(resolve, 100));
-        }
-    }
-*/
-
     async #playloop(){        
         this.#running = true;
-        console.log('start playloop');
         while(!this.closed && this.#running){
-            console.log('before getStatus');
             let ready = await this.getStatus();
-            console.log('after getStatus');
             if (ready){
                 if (this.onFrame){
-                    console.log('before onFrame');
                     await this.onFrame(this);
-                    console.log('after onFrame');
                 }
             }
         }
-        console.log('stop playloop');
     }
 
     async #getFirmwareVersion() {
@@ -343,10 +266,9 @@ export class HeliosDevice{
                 if (result.status=='ok' && result.data && result.data.getUint8(0) === HELIOS_STATUS_RESPONSE_CODE) {
                     if (result.data.getUint8(1) === 1){
                         return HELIOS_SUCCESS;
-                    } else {
-                        retry--;
                     }
-                }				
+                }	
+                retry--;			
             }
             return HELIOS_FAIL;
         } catch (error) {
@@ -381,7 +303,6 @@ export class HeliosDevice{
     }
 
     async stop() {
-        this.#running = false;
         if (this.closed) return HELIOS_ERROR;
         const buffer = new Uint8Array(2);
         buffer[0] = HELIOS_STOP_COMMAND;
@@ -431,6 +352,8 @@ export class HeliosDevice{
 
         try {
             await this.stop();
+            this.#running = false;
+            await new Promise(resolve => setTimeout(resolve, 100)); 
             await this.usbDevice.close();
             this.closed = true;
         } catch (error) {
